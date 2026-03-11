be689f6970443a97432cba4553dd46fd742feeaf
BlobRaccoon <sjrogers2024@gmail.com>
2026-03-11 16:49:34 -0600

    Update and rename BlobMath.js to triangulet prices.js

---
+++ triangulet prices.js
@@ -0,0 +1,131 @@
+               Triangulet prices
+               by ​​̳𝚋​̳𝚕​̳𝚘​̳𝚋​̳_​̳𝚛​̳𝚊​̳𝚌​̳𝚌​̳𝚘​̳𝚘​̳𝚗​̳
+
+Capsule:   Trian:             Net price:      Open price:
+-----------------------------------------------------------
+Emoji      Sad Eyes Emoji     -134            139
+Emoji      Skull Emoji        -134            139
+Emoji      Rich Emoji         -134            139
+Emoji      Angry Emoji        -427            447
+Emoji      Eyebrow Emoji      -427            447
+Emoji      Cool Emoji         -427            447
+Emoji      Flushed Emoji      -550            625
+Emoji      Dead Emoji         -550            625
+Emoji      Devil Emoji        -550            625
+Emoji      Poop Emoji         -6,466          6,666
+Emoji      Laughing Emoji     -6,466          6,666
+Emoji      Cold Emoji         -85,414         85,714
+Emoji      Hot Emoji          -85,414         85,714
+Emoji      Clown Emoji        499,000,000     1,000,000
+
+Hat        Blue Cap           -236            241
+Hat        Green Cap          -236            241
+Hat        Red Cap            -275            280
+Hat        Grey Cap           -275            280
+Hat        Cowboy Hat         -330            350
+Hat        Straw Hat          -330            350
+Hat        Sombrero           -425            500
+Hat        Top Hat            -439            514
+Hat        Fedora             -3,300          3,500
+Hat        Golden Top Hat     -174,700        175,000
+Hat        Diamond Hat        556,500,000     3,500,000
+
+OG         Trixter            -115            120
+OG         BRP                -115            120
+OG         Xotic              -180            200
+OG         LukeDuke           -925            1,000
+OG         Hans Island        -925            1,000
+OG         EvDogg09           -2,800          3,000
+OG         ColControl         -59,700         60,000
+OG         Bringles           -59,700         60,000
+OG         monkey             763,000,000     3,000,000
+OG         penguinpowers      763,000,000     3,000,000
+
+Hacker     Hacker             -161            166
+Hacker     ERROR              -161            166
+Hacker     Virus              -161            166
+Hacker     Banned             -161            166
+Hacker     Hacker Bot         -230            250
+Hacker     Hacker Code        -292            312
+Hacker     Glitch             -396            416
+Hacker     Bomb               -758            833
+Hacker     Letter             -758            833
+Hacker     Idiot              -2,300          2,500
+Hacker     Lovely Letter      -49,700         50,000
+Hacker     Overload           -249,700        250,000
+Hacker     Dark Web           663,500,000     2,500,000
+
+Chess      White Pawn         -126            131
+Chess      Black Pawn         -126            131
+Chess      White Knight       -126            131
+Chess      Black Knight       -126            131
+Chess      White Bishop       -230            250
+Chess      Black Bishop       -230            250
+Chess      White Rook         -425            500
+Chess      Black Rook         -425            500
+Chess      White Queen        -4,800          5,000
+Chess      Black Queen        -4,800          5,000
+Chess      White King         -83,033         83,333
+Chess      Black King         -83,033         83,333
+Chess      Pink Knight        47,500,000      2,500,000
+
+Space      Earth              -126            131
+Space      Moon               -126            131
+Space      Mars               -187            192
+Space      Saturn             -230            250
+Space      Neptune            -230            250
+Space      Uranus             -425            500
+Space      Rocket             -425            500
+Space      Sun                -4,800          5,000
+Space      Galaxy             -4,800          5,000
+Space      Tim the Alien      -83,033         83,333
+Space      Northern Lights    -83,033         83,333
+Space      Black Hole         -83,033         83,333
+Space      Meteor             797,500,000     2,500,000
+
+Color      Red                -75             80
+Color      Blue               -75             80
+Color      Yellow             -75             80
+Color      Orange             -113            133
+Color      Purple             -133            153
+Color      Green              -180            200
+Color      Black              -591            666
+Color      Brown              -925            1,000
+Color      Pink               -1,800          2,000
+Color      White              -1,800          2,000
+Color      Silver             -3,700          4,000
+Color      Gold               -19,700         20,000
+Color      Rainbow            360,000,000     2,000,000
+
+Food       Watermelon         -106            111
+Food       Carrot             -106            111
+Food       Cheese             -112            117
+Food       Pizza              -112            117
+Food       Cookie             -120            125
+Food       Strawberry         -120            125
+Food       Pastry             -180            200
+Food       Popcicle           -253            273
+Food       Donut              -257            277
+Food       Pie                -261            281
+Food       Waffle             -265            285
+Food       Candy Corn         -346            421
+Food       Chocolate Cake     -346            421
+Food       Sandwich           -3,800          4,000
+Food       Vanilla Cake       -19,800         20,000
+Food       Chocolate Bar      -21,922         22,222
+Food       Golden Carrot      -33,033         33,333
+Food       Kiss               399,710,145     289,855
+
+Sports     Soccer Ball        -100            105
+Sports     Football           -100            105
+Sports     Basketball         -100            105
+Sports     Baseball           -100            105
+Sports     Boomerang          -180            200
+Sports     Volleyball         -180            200
+Sports     Tennis Ball        -180            200
+Sports     Pool Rack          -325            400
+Sports     Bowling Ball       -925            1,000
+Sports     Birdie             -3,800          4,000
+Sports     Beach Ball         -66,366         66,666
+Sports     Gold Bowling Ball  315,600,000     400,000
+
